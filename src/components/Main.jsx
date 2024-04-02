a999296629e87149290d7f5886220ed3ecdc03ec
NguyenDucHuy <duchuytv2102@gmail.com>
2024-04-02 18:49:43 +0700

    add generative functionality, pre-loader animation: 100%

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { assets } from '../assets/exportAssets';
 import './Main.css';
+import { Context } from '../context/Context';
+import DOMPurify from 'dompurify';
 
 const Main = () => {
+    const { onSent, recentPrompt, showResult, loading, data, search, setSearch } = useContext(Context);
+
     return (
         <div className='flex-1 min-h-[100vh] pb-[15vh] relative'>
             {/* nav */}
@@ -13,42 +17,70 @@ const Main = () => {
 
             {/* container */}
             <div className='max-w-[900px] m-auto'>
-                {/* say hi */}
-                <div className='greet text-[56px] text-[#c4c7c5] font-[500] p-[20px]'>
-                    <p>
-                        <span>Hello, Duc Huy.</span>
-                    </p>
-                    <p>How I can help you today?</p>
-                </div>
+                {!showResult ? (
+                    <>
+                        {/* say hi */}
+                        <div className='greet text-[56px] text-[#c4c7c5] font-[500] p-[20px]'>
+                            <p>
+                                <span>Hello, Duc Huy.</span>
+                            </p>
+                            <p>How I can help you today?</p>
+                        </div>
 
-                {/* 4 cards */}
-                <div className='cards'>
-                    <div className='card'>
-                        <p>Cristiano Ronaldo comeback to Man Utd</p>
-                        <img src={assets.compass_icon} alt='' />
+                        {/* 4 cards */}
+                        <div className='cards'>
+                            <div className='card'>
+                                <p>Cristiano Ronaldo comeback to Man Utd</p>
+                                <img src={assets.compass_icon} alt='' />
+                            </div>
+                            <div className='card'>
+                                <p>4 tricks to master your English speaking skill</p>
+                                <img src={assets.bulb_icon} alt='' />
+                            </div>
+                            <div className='card'>
+                                <p>The future of AI. What is happened with OpenAI</p>
+                                <img src={assets.message_icon} alt='' />
+                            </div>
+                            <div className='card'>
+                                <p>How long to learn a new programming language?</p>
+                                <img src={assets.code_icon} alt='' />
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <div className='result'>
+                        <div className='result-title'>
+                            <img src={assets.user_icon} alt='' />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className='result-content'>
+                            <img src={assets.gemini_icon} alt='' />
+                            {loading ? (
+                                <div className='loader'>
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div>
+                            ) : (
+                                <p dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data) }}></p>
+                            )}
+                        </div>
                     </div>
-                    <div className='card'>
-                        <p>4 tricks to master your English speaking skill</p>
-                        <img src={assets.bulb_icon} alt='' />
-                    </div>
-                    <div className='card'>
-                        <p>The future of AI. What is happened with OpenAI</p>
-                        <img src={assets.message_icon} alt='' />
-                    </div>
-                    <div className='card'>
-                        <p>How long to learn a new programming language?</p>
-                        <img src={assets.code_icon} alt='' />
-                    </div>
-                </div>
+                )}
 
                 {/* search */}
                 <div className='main-bottom'>
                     <div className='search-box'>
-                        <input type='text' placeholder='Enter a prompt here' />
+                        <input
+                            onChange={(e) => setSearch(e.target.value)}
+                            value={search}
+                            type='text'
+                            placeholder='Enter a prompt here'
+                        />
                         <div className='flex gap-3'>
                             <img src={assets.gallery_icon} alt='' />
                             <img src={assets.mic_icon} alt='' />
-                            <img src={assets.send_icon} alt='' />
+                            <img onClick={() => onSent()} src={assets.send_icon} alt='' />
                         </div>
                     </div>
                     <p className='bottom-info'>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,3 +1,41 @@
-import { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import runChat from '../config/gemini';
 
 export const Context = createContext();
+
+const ContextProvider = (props) => {
+    const [search, setSearch] = useState('');
+    const [recentPrompt, setRecentPrompt] = useState('');
+    const [prevPrompts, setPrevPrompts] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState('');
+
+    const onSent = async (prompt) => {
+        setData('');
+        setLoading(true);
+        setShowResult(true);
+        setRecentPrompt(search);
+        const res = await runChat(search);
+        setData(res);
+        setLoading(false);
+        setSearch('');
+    };
+    // onSent('Do you know Cristiano Ronaldo?');
+
+    const contextValue = {
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        data,
+        search,
+        setSearch,
+    };
+    return <Context.Provider value={contextValue}>{props.children}</Context.Provider>;
+};
+
+export default ContextProvider;
